9bdc49036f91bb9150937ff1b99586876af971ea
Solyomka <andreasubaine@gmail.com>
2026-05-22 18:49:21 +0200

    Post-Get-Delete functions are added.

--- usersApi/Controllers/UserSqlController.cs
+++ usersApi/Controllers/UserSqlController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using usersApi.Models;
 
 namespace usersApi.Controllers
 {
@@ -7,11 +9,109 @@ namespace usersApi.Controllers
     [ApiController]
     public class UserSqlController : ControllerBase
     {
-       
+        Connect conn = new Connect();
+
         [HttpPost]
-        public object AddNewUser()
+        public ActionResult AddNewUser([FromBody]Users users)
         {
-            return new { message = "Hello"};
+            try
+            {
+                var user = new Users
+                {
+                    UserName = users.UserName,
+                    Email = users.Email,
+                    Password = users.Password
+                };
+
+                conn.MySqlConnection.Open();
+
+                string sql = $"INSERT INTO `users`(`username`, `email`, `password`, `createdtime`) VALUES (@username,@email,@password,@createdtime)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.MySqlConnection);
+
+                cmd.Parameters.AddWithValue("@username", user.UserName);
+                cmd.Parameters.AddWithValue("@email", user.Email);
+                cmd.Parameters.AddWithValue("@password", user.Password);
+                cmd.Parameters.AddWithValue("@createdtime", user.CreatedTime);
+
+                cmd.ExecuteNonQuery();
+
+                conn.MySqlConnection.Close();
+
+                return StatusCode(201, new { message = "Sikeres felvétel!" });
+
+
+               
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, new { message = ex.Message });
+            }
+          
+        }
+       
+        [HttpGet]
+        public ActionResult GetAllUser()
+        {
+
+            List<Users> users = new List<Users>();
+            try
+            {
+                conn.MySqlConnection.Open();
+
+                string sql = "SELECT * FROM users";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.MySqlConnection);
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    var user = new Users
+                    {
+                        Id = dr.GetInt32(0),
+                        UserName = dr.GetString("username"),
+                        Email = dr.GetString("email"),
+                        Password = dr.GetString("password")
+                       
+                    };
+
+                    users.Add(user);
+                }
+                conn.MySqlConnection.Close();
+
+                return StatusCode(200, new { message = "Sikeres lekésrdezés.", result = users });
+
+               
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUser(int id)
+        {
+            try
+            {
+                conn.MySqlConnection.Open();
+
+                string sql = "DELETE FROM users WHERE id = @id";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.MySqlConnection);
+
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+
+                conn.MySqlConnection.Close();
+
+                return StatusCode(200, new { message = "Sikeres törlés."});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, new { message = ex.Message });
+            }
         }
     }
 }
--- usersApi/obj/Debug/net10.0/usersApi.AssemblyInfo.cs
+++ usersApi/obj/Debug/net10.0/usersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("usersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b7f0f108ce1f822ad49f6e8f536f1ed0d1de9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("usersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("usersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
